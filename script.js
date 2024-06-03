92da92ebb5544ead750c8ccaee0624bd7d8eb5a9
Matteo Fusillo <58771053+mfusillo98@users.noreply.github.com>
2024-06-03 08:27:00 +0200

    + Service worker

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then((registration) => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, (error) => {
+                console.log('ServiceWorker registration failed: ', error);
+            });
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const homeSection = document.getElementById('home-section');
     const transactionsSection = document.getElementById('transactions-section');
---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'budget-manager-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    '/favicon/favicon.ico',
+    '/favicon/favicon-16x16.png',
+    '/favicon/favicon-32x32.png',
+    '/favicon/android-chrome-192x192.png',
+    '/favicon/android-chrome-512x512.png',
+    '/favicon/apple-touch-icon.png',
+    '/favicon/mstile-150x150.png',
+    '/favicon/safari-pinned-tab.svg',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
